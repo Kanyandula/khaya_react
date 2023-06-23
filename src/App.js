638176e40dcf9a9927e9aedd09e0dd77b3d2bb44
Ephraim Kanyandula <ekanyandula8@gmail.com>
2023-06-23 22:14:37 +0100

    create the header component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-
-//import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -7,10 +5,14 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
+
+
 
 function App() {
   return (
    <Router>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/profile" element={<Profile/>} />
---
+++ src/components/Header.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import LogoIcon from './logo.svg';
+import { useLocation, useNavigate} from "react-router-dom"
+export default function Header() {
+  const location = useLocation();
+  const navigate = useNavigate()
+  function pathMathRoute(route){
+    if(route === location.pathname){
+      return true
+    }
+
+  }
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0
+    z-50">
+           <header className="flex justify-between items-center
+           px-3 max-w-6xl mx-auto">
+            <div >
+            <img src={LogoIcon} alt="logo"
+            className="h-12 cursor-pointer "
+            onClick={()=>navigate("/")}
+             />
+            </div>
+            <div>
+              <ul className="flex space-x-10">
+                <li className={`cursor-pointer py-3 text-sm font-semibold
+                text-gray-400 border-b-[3px]
+                border-b-transparent
+                ${pathMathRoute("/") && "text-black border-b-blue-500"}`}
+                onClick={()=>navigate("/")}
+                >Home</li>
+                <li
+                className={`cursor-pointer py-3 text-sm font-semibold
+                text-gray-400 border-b-[3px]
+                border-b-transparent
+                ${pathMathRoute("/offers") && "text-black border-b-blue-500"}`}
+                onClick={()=>navigate("/offers")}
+                >Offers</li>
+                <li
+                className={`cursor-pointer py-3 text-sm font-semibold
+                text-gray-400 border-b-[3px]
+                border-b-transparent
+                ${pathMathRoute("/sign-in") && "text-black border-b-blue-500"}`}
+                onClick={()=>navigate("/sign-in")}
+                >Sign in</li>
+                {/* <li
+                className={`cursor-pointer py-3 text-sm font-semibold
+                text-gray-400 border-b-[3px]
+                border-b-transparent
+                ${pathMathRoute("/sign-up") && "text-black border-b-blue-500"}`}>SignUp</li>
+                <li
+                className={`cursor-pointer py-3 text-sm font-semibold
+                text-gray-400 border-b-[3px]
+                border-b-transparent
+                ${pathMathRoute("/forgot-password") && "text-black border-b-blue-500"}`}>ForgotPassword</li>
+                <li
+                className={`cursor-pointer py-3 text-sm font-semibold
+                text-gray-400 border-b-[3px]
+                border-b-transparent
+                ${pathMathRoute("/profile") && "text-black border-b-blue-500"}`}>Profile</li> */}
+              </ul>
+            </div>
+           </header>
+    </div>
+  )
+}
